459fb406a36156a5332f25921f79f1126d1876a8
Shamil Sadigov <sov.shamil@gmail.com>
2022-06-10 21:08:07 +0400

    Upgrade service to .NET 6

--- ReportService/ReportService/Startup.cs
+++ ReportService/ReportService/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using ReportService.Services;
 
 namespace ReportService
@@ -23,7 +24,7 @@ namespace ReportService
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
