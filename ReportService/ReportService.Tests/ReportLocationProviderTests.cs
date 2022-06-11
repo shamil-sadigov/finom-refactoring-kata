e4b1c38068a01f9729b118abb71bd12179ede7ef
Shamil Sadigov <sov.shamil@gmail.com>
2022-06-11 22:48:05 +0400

    Add ReportLocationProvider tests

---
+++ ReportService/ReportService.Tests/ReportLocationProviderTests.cs
@@ -0,0 +1,43 @@
+
+using FluentAssertions;
+using ReportService.Services.Report;
+using Xunit.Abstractions;
+
+namespace ReportService.Tests;
+
+public class ReportLocationProviderTests
+{
+    [Theory]
+    [InlineData(2020, 09)]
+    [InlineData(2018, 05)]
+    [InlineData(2010, 12)]
+    public void Should_return_expected_report_location(int year, int month)
+    {
+        // Arrange
+        var pathBuilder = new ReportLocationProvider();
+
+        var expectedPath = Path.Combine(
+            Directory.GetCurrentDirectory(), 
+            @$"reports\2020\accounting-report-{year}-{month}.txt");
+        
+        // Act
+        var filePath = pathBuilder.GetReportLocation(year, month);
+        
+        // Assert
+        filePath.Should().Be(expectedPath);
+    }
+    
+    [Theory]
+    [InlineData(-12, 0)]
+    [InlineData(0, 05)]
+    [InlineData(2010, 15)]
+    [InlineData(2010, -3)]
+    public void Cannot_get_report_location_when_provided_time_is_invalid(int year, int month)
+    {
+        var pathBuilder = new ReportLocationProvider();
+        
+        pathBuilder.Invoking(x => x.GetReportLocation(year, month))
+            .Should()
+            .Throw<ArgumentOutOfRangeException>();
+    }
+}
