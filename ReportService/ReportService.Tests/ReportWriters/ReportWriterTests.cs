be91892a458fe85f5461182be50823987add71d9
Shamil Sadigov <sov.shamil@gmail.com>
2022-06-12 00:12:10 +0400

    Add ReportWriterTests

---
+++ ReportService/ReportService.Tests/ReportWriters/ReportWriterTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using ReportService.Domain;
+using ReportService.Services.Report;
+using Xunit.Abstractions;
+
+namespace ReportService.Tests.ReportWriters;
+
+public class ReportWriterTests
+{
+    [Fact]
+    public async Task Should_write_expected_report()
+    {
+        // Arrange
+        var employees = CreateSampleEmployees();
+        var stringWriter = new StringWriter();
+        var reportWriter = new ReportWriter();
+        var expectedReport = await GetExpectedReport();
+
+        // Act
+        await reportWriter.WriteReportAsync(stringWriter, 2020, 10, employees);
+        var actualReport = stringWriter.ToString();
+
+        // Assert
+        actualReport.Should().Be(expectedReport);
+    }
+
+    private static List<EmployeeReportItem> CreateSampleEmployees()
+    {
+        var employees = new List<EmployeeReportItem>
+        {
+            new("Ernest Hemingway", "1", "Писатели", 2000),
+
+            new("John Fogerty", "3", "Музыканты", 5000),
+            new("David Gilmour", "4", "Музыканты", 5000),
+
+            new("Charles Babbage", "6", "IT", 6000),
+            new("Ada Lovelace", "7", "IT", 6000),
+            new("Tim Berners Lee", "5", "IT", 8000),
+        };
+        return employees;
+    }
+
+    private static async Task<string> GetExpectedReport()
+    {
+        var report = Path.Combine(Directory.GetCurrentDirectory(), "ReportWriters\\Expected_report.txt");
+
+        var expectedReport = await File.ReadAllTextAsync(report);
+        return expectedReport;
+    }
+}
