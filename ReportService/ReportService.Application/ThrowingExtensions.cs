98edc89d220f7ab20e30aca940320b89b42c3ab2
Shamil Sadigov <sov.shamil@gmail.com>
2022-06-12 22:46:24 +0400

    Delete unused helper method

--- ReportService/ReportService.Application/ThrowingExtensions.cs
+++ ReportService/ReportService.Application/ThrowingExtensions.cs
@@ -4,19 +4,11 @@ namespace ReportService.Application;
 
 public static class ThrowingExtensions
 {
-    public static string ThrowIfNull(
+    public static void ThrowIfNull(
         this string str,
         [CallerArgumentExpression("str")] string argName = "")
     {
-        if (string.IsNullOrWhiteSpace(str)) throw new ArgumentNullException(argName);
-
-        return str;
-    }
-
-    public static void ThrowIfNull<T>(
-        this T obj,
-        [CallerArgumentExpression("obj")] string argName = "")
-    {
-        if (obj is null) throw new ArgumentNullException(argName);
+        if (string.IsNullOrWhiteSpace(str)) 
+            throw new ArgumentNullException(argName);
     }
 }
